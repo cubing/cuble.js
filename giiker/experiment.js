8f97d937174bc40a4d1451ea6e997cfceccb53a5
Lucas Garron <code@garron.net>
2018-06-27 16:01:50 -0700

    Initial npm package.

--- giiker/experiment.js
+++
@@ -1,7 +0,0 @@
-
-var cube = new GiikerCube();
-window.addEventListener("load", function() {
-  document.querySelector("#connect").addEventListener("click", function() {
-    cube.connect();
-  });
-});
