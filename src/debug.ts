254626b97bcb6317890516cbef59e11e9471ef6b
Lucas Garron <code@garron.net>
2018-09-19 23:27:10 -0700

    Disable debug log messsages by default, but support re-enabling.

--- src/debug.ts
+++ src/debug.ts
@@ -1,6 +1,14 @@
+var DEBUG_LOGGING_ENABLED = false;
 
-// TODO: Support disabling.
+export function enableDebugLogging(enable: boolean): void {
+  DEBUG_LOGGING_ENABLED = enable;
+}
+
 export function debugLog(...args: any[]) {
+  if (!DEBUG_LOGGING_ENABLED) {
+    return;
+  }
+
   if (console.info) {
     console.info.apply(console, args);
   } else {
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {GiiKERi3Cube} from "./giiker"
 export {BluetoothPuzzle, connect, MoveEvent} from "./bluetooth-puzzle";
 export {KeyboardPuzzle, debugKeyboardConnect} from "./keyboard"
+export {enableDebugLogging} from "./debug"
